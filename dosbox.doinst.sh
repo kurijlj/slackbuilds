78756d7154723d9d6b3dcbaba309d82924e5001d
Ljubomir Kurij <kurijlj@gmail.com>
2017-08-31 16:57:15 +0200

    Added rclone build script.

--- dosbox.doinst.sh
+++
@@ -1,5 +0,0 @@
-if [ -x usr/bin/update-desktop-database ]; then
-    usr/bin/update-desktop-database -q usr/share/applications
-    usr/bin/update-desktop-database -q usr/local/share/applications
-fi
-
--- skype.doinst.sh
+++
@@ -1,5 +0,0 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
-  /usr/bin/update-desktop-database -q usr/local/share/applications >/dev/null 2>&1
-fi
-
